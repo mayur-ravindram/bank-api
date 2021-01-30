9e78b20189b9f0467cd2c7e9f70a507cba440f03
mayureshkulkarni43 <mayureshkulkarni43@gmail.com>
2021-01-30 17:17:34 +0530

    my commit with new code

---
+++ src/main/java/com/bank/DefaultController.java
@@ -0,0 +1,14 @@
+package com.bank;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DefaultController {
+
+	@GetMapping("/ping")
+	public String getHealth() {
+		return "Success";
+		
+	}
+}
